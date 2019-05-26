217df19df083b174a5ffc20fd20be934d76c4950
Abdelaziz <abdelazizMalfy@gmail.com>
2019-05-26 03:47:35 +0200

    Prepared for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    mongoURI : 'mongodb+srv://abdelaziz:abdelaziz@devconnector-m8h1y.mongodb.net/test?retryWrites=true',
-    secretOrKey:'Amazing'   
+if(process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod')
+} else {
+    module.exports = require('./keys_dev')
 }
